4026336c078724f5d2b9bcf73e4a485e857b404d
Irwing <irwingcorrea95@gmail.com>
2024-01-25 14:58:25 -0300

    test- add consultarTudoTest e atualizarTest para Produto

--- src/test/java/br/com/icorrea/ProdutoTeste.java
+++ src/test/java/br/com/icorrea/ProdutoTeste.java
@@ -2,7 +2,10 @@ package br.com.icorrea;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+
+import java.util.List;
 
 import org.junit.Test;
 
@@ -69,6 +72,70 @@ public class ProdutoTeste {
 		assertNotNull(prodBD);
 		assertEquals(prod.getCodigo(), prodBD.getCodigo());
 		assertEquals(prod.getNome(), prodBD.getNome());
+		
+		Integer countDel = dao.excluir(prodBD);
+		assertTrue(countDel == 1);
+	}
+	
+	@Test
+	public void consultarTudoTest() throws Exception {
+		ProdutoDAO dao = new ProdutoDAO();
+		
+		Produto prod = new Produto();
+		prod.setCodigo("01");
+		prod.setNome("Produto 01");
+		Integer countCad = dao.cadastrar(prod);
+		assertTrue(countCad == 1);
+		
+		Produto outroProd = new Produto();
+		outroProd.setCodigo("02");
+		outroProd.setNome("Produto 02");
+		Integer countCad2 = dao.cadastrar(outroProd);
+		assertTrue(countCad2 == 1);
+		
+		List<Produto> list = dao.consultarTudo();
+		assertNotNull(list);
+		assertEquals(2, list.size());
+		
+		int countDel = 0;
+		for(Produto p : list) {
+			dao.excluir(p);
+			countDel++;
+		}
+		assertEquals(list.size(), countDel);
+		
+		list = dao.consultarTudo();
+		assertEquals(list.size(), 0);
+	}
+	
+	@Test
+	public void atualizarTest() throws Exception {
+		ProdutoDAO dao = new ProdutoDAO();
+		
+		Produto prod = new Produto();
+		prod.setCodigo("01");
+		prod.setNome("Produto 01");
+		Integer countCad = dao.cadastrar(prod);
+		assertTrue(countCad == 1);
+		
+		Produto prodBD = dao.consultar("01");
+		assertNotNull(prodBD);
+		assertEquals(prod.getCodigo(), prodBD.getCodigo());
+		assertEquals(prod.getNome(), prodBD.getNome());
+		
+		prodBD.setCodigo("02");
+		prodBD.setNome("Produto 02");
+		Integer countUpdate = dao.atualizar(prodBD);
+		assertTrue(countUpdate == 1);
+		
+		Produto consult01 = dao.consultar("01");
+		assertNull(consult01);
+		
+		Produto prodBD02 = dao.consultar("02");
+		assertNotNull(prodBD02);
+		assertEquals(prodBD.getId(), prodBD02.getId());
+		assertEquals(prodBD.getNome(), prodBD02.getNome());
+		assertEquals(prodBD.getCodigo(), prodBD02.getCodigo());
 		
 		Integer countDel = dao.excluir(prodBD);
 		assertTrue(countDel == 1);
